62b5ee7774b3cbf1dff8881c6c1db53f7b93da83
RenatoGigantino <148282134+RenatoGigantino@users.noreply.github.com>
2023-12-03 12:38:38 +0100

    Delete static/register.js

--- static/register.js
+++
@@ -1,7 +0,0 @@
-let bottone = document.getElementById("registrati")
-bottone.addEventListener("click", register)
-function register (e) {
-    e.preventDefault()
-    let username = document.getElementById("username")
-    console.log(username.value)
-}
