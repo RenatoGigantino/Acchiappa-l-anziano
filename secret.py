8a1ef7a106eb049ed679f4074439cf2c61aee458
RenatoGigantino <148282134+RenatoGigantino@users.noreply.github.com>
2023-12-03 12:45:08 +0100

    Add files via upload

---
+++ secret.py
@@ -0,0 +1,4 @@
+secret_key = "a9354c0aa28dc7c28ea075e7f5a87d83b5ff8b46118ecbb4d65831a48ef02318"
+api_key = "AIzaSyAuHfk-HjiVyHzR1pO99qBajq6Q8oAvO54"
+# No traditional database password for the (default) user
+database_password = None
