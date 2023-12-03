adeb0239c3ac66ffb9d54ace3eed3b56bcc7c72b
RenatoGigantino <148282134+RenatoGigantino@users.noreply.github.com>
2023-12-03 12:38:14 +0100

    Delete static/login.js

--- static/login.js
+++
@@ -1,9 +0,0 @@
-let bottone = document.getElementById("accedi")
-bottone.addEventListener("click", login)
-function login (e) {
-    e.preventDefault()
-    let username = document.getElementById("username")
-    console.log(username.value)
-    let password = document.getElementById("password")
-    console.log(password.value)
-}
